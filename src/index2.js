092052041b456e41816fc188dd49796ee3128969
Sol <solemich@gmail.com>
2022-05-10 23:52:25 -0400

    Modificado

--- src/index2.js
+++ src/index2.js
@@ -1,101 +1,81 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
+document.getElementById("btn3").addEventListener("click", function () {
+  let text1 = document.getElementById("text1").value;
+  let shift = parseInt(document.getElementById("shift").value);
+  // parseint es para transformar a numero.
+  // alert(text1);
+  // alert(shift);
+  // console.log(typeof shift)
+  let resultado = cipher.encode(shift, text1);
+  // alert(resultado);
+  document.getElementById("encrypt").value = resultado;
 
+  //shift correcto: 2
 
+  if (resultado == "PQ RWGFGU UCNKT") {
+    alert(
+      "NO CREAS QUE PODRÁS ESCAPAR, INTENTA CODIFICAR LA SEGUNDA FRASE OCULTA EN LAS INSTRUCCIONES"
+    );
+  }
+  //shifts incorrectos: 1, 3, 4, 5
 
-document.getElementById("btn3").addEventListener("click",
-function () {
-let text1 = document.getElementById("text1").value;
-let shift = parseInt(document.getElementById("shift").value);
-// parseint es para transformar a numero.
-// alert(text1);
-// alert(shift);
-// console.log(typeof shift)
- let resultado =cipher.encode(shift,text1);
-// alert(resultado);
-document.getElementById("encrypt").value = resultado;
+  if (resultado == "OP QVFEFT TBMJS") {
+    alert("WARNING");
+  }
 
+  if (resultado == "QR SXHGHV VDOLU ") {
+    alert("WARNING");
+  }
 
-
-//shift correcto: 2
-
-if (resultado == "PQ RWGFGU UCNKT"){ 
-alert("NO CREAS QUE PODRÁS ESCAPAR, INTENTA CODIFICAR LA SEGUNDA FRASE OCULTA EN LAS INSTRUCCIONES"); 
-    
-}
-//shifts incorrectos: 1, 3, 4, 5
-
-if (resultado == "OP QVFEFT TBMJS"){ 
-    alert("WARNING"); 
-        
-    }
-
-if (resultado == "QR SXHGHV VDOLU "){ 
-        alert("WARNING"); 
-            
-    }
-
-if (resultado == "RS TYIHIW WEPMV"){ 
-        alert("WARNING"); 
-            
-    }
-if (resultado == "ST UZJIJX XFQNW"){ 
-        alert("WARNING"); 
-            
-    }    
-});   
+  if (resultado == "RS TYIHIW WEPMV") {
+    alert("WARNING");
+  }
+  if (resultado == "ST UZJIJX XFQNW") {
+    alert("WARNING");
+  }
+});
 
 //para mayusculas cuando se ingrese a texto
 
 let text1 = document.getElementById("text1");
-text1.addEventListener("keyup", ()=>{
-text1.value = text1.value.toUpperCase()
+text1.addEventListener("keyup", () => {
+  text1.value = text1.value.toUpperCase();
 });
 
+document.getElementById("btn4").addEventListener("click", function () {
+  let text2 = document.getElementById("text2").value;
+  let shift2 = parseInt(document.getElementById("shift2").value);
+  let resultado = cipher.decode(shift2, text2);
+  // alert(resultado);
+  document.getElementById("encrypt2").value = resultado;
 
-document.getElementById("btn4").addEventListener("click",
-function () {
-let text2 = document.getElementById("text2").value;
-let shift2 = parseInt(document.getElementById("shift2").value);
- let resultado =cipher.decode(shift2,text2);
-// alert(resultado);
-document.getElementById("encrypt2").value = resultado;
+  // decodificar "DGLRV"
+  //Shift 3
+  if (resultado == "ADIOS") {
+    alert("UNA VICTORIA NO ES NADA, NOS VEMOS EN LA SIGUIENTE PRUEBA, HUMANO");
+    window.location = "win.html";
+  }
 
-// decodificar "DGLRV"
-//Shift 3
-if (resultado == "ADIOS"){ 
-    alert("UNA VICTORIA NO ES NADA, NOS VEMOS EN LA SIGUIENTE PRUEBA, HUMANO")
-    window.location="win.html";
-        
-}    
+  //shift incorrectos 1,2
 
-//shift incorrectos 1,2
+  if (resultado == "CFKQU") {
+    alert("ADIOS HUMANO");
+    window.location = "GAMEOVER.html";
+  }
 
-if (resultado == "CFKQU"){ 
-    alert("ADIOS HUMANO")
-    window.location="GAMEOVER.html";
-        
-}
-
-if (resultado == "BEJPT"){ 
-    alert("ADIOS HUMANO")
-    window.location="GAMEOVER.html";
-        
-}
-
-
-    
-
+  if (resultado == "BEJPT") {
+    alert("ADIOS HUMANO");
+    window.location = "GAMEOVER.html";
+  }
 });
 
 //para mayusculas cuando se ingrese a texto
 
 let text2 = document.getElementById("text2");
-text2.addEventListener("keyup", ()=>{
-text2.value = text2.value.toUpperCase()
+text2.addEventListener("keyup", () => {
+  text2.value = text2.value.toUpperCase();
 });
-
-
 
 // funcion para verificar que el texto está entrando
 
